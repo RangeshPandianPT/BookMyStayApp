5020ff0f0a6b9de9f0a3a570eaa5be894bc7897e
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2026-04-01 09:07:48 +0530

    Use Case 11: Concurrent Booking Simulation (Thread Safety)

--- BookMyStayApp/src/BookingRequestQueue.java
+++ BookMyStayApp/src/BookingRequestQueue.java
@@ -1,15 +1,25 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class BookingRequestQueue {
+class BookingRequestQueue {
 
-    private Queue<String> queue = new LinkedList<>();
+    private Queue<BookingRequest> queue = new LinkedList<>();
 
-    public void addRequest(String request) {
+    // Add request (Producer)
+    public synchronized void addRequest(BookingRequest request) {
         queue.add(request);
+        notify(); // Wake up waiting threads
     }
 
-    public String processRequest() {
+    // Get request (Consumer)
+    public synchronized BookingRequest getRequest() {
+        while (queue.isEmpty()) {
+            try {
+                wait(); // Wait until request arrives
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
         return queue.poll();
     }
 }
